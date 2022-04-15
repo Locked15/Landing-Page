01089f24d4f94134ceaa9b3e239524085c5f697f
Алмаз <almgal353@gmail.com>
2022-04-15 19:51:44 +0500

    Added 'Benefits' component.

--- src/Components/Body/MainContent.js
+++ src/Components/Body/MainContent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutCompany from './AboutCompany';
+import Benefits from './Benefits';
 import HotToursFeatures from './HotToursFeatures';
 
 function MainContent() {
     return (
         <main style={{maxWidth: '57.25%'}}>
             <AboutCompany />
+            <Benefits />
 
             <HotToursFeatures />
         </main>
