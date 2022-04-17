6ecb1860360f44c52929fd90e74850a75050a73b
Алмаз <almgal353@gmail.com>
2022-04-17 10:15:55 +0500

    Added images. Side galleries expanded.

--- src/Components/BorderElements/LeftSideGalleryElements.js
+++ src/Components/BorderElements/LeftSideGalleryElements.js
@@ -1,50 +1,113 @@
 import React from "react";
-import './Styles/style.css';
+import "./Styles/style.css";
 
 function LeftSidesGalleryElement() {
 	return (
-		<aside id="leftPanel" className="aside-block gallery border-right">
+		<aside id='leftPanel' className='aside-block gallery border-right'>
 			<figure>
-				<img src={require('../../resources/Images/CrimeaTours.jpg')} alt='Крым.'/>
+				<img
+					src={require("../../resources/Images/CrimeaTours.jpg")}
+					alt='Крым.'
+				/>
 
 				<figcaption>Крым.</figcaption>
 			</figure>
 
 			<figure>
-                <img src={require('../../resources/Images/SochiPhoto.jpg')} alt='Сочи.'/>
+				<img
+					src={require("../../resources/Images/SochiPhoto.jpg")}
+					alt='Сочи.'
+				/>
 
-                <figcaption>Сочи.</figcaption>
+				<figcaption>Сочи.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/Spain-1024x768.jpeg')} alt='Испания.'/>
+				<img
+					src={require("../../resources/Images/Spain-1024x768.jpeg")}
+					alt='Испания.'
+				/>
 
 				<figcaption>Испания.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/HotTous.jpg')} alt='Курорт.'/>
+				<img
+					src={require("../../resources/Images/HotTous.jpg")}
+					alt='Курорт.'
+				/>
 
 				<figcaption>Курорт.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/Rossiya-1.jpg')} alt='Москва.'/>
+				<img
+					src={require("../../resources/Images/Rossiya-1.jpg")}
+					alt='Москва.'
+				/>
 
 				<figcaption>Москва.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/goryashchiye-tury-na-Kubu.jpg')} alt='Куба.'/>
+				<img
+					src={require("../../resources/Images/goryashchiye-tury-na-Kubu.jpg")}
+					alt='Куба.'
+				/>
 
 				<figcaption>Куба.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/e644b78ea1be3d6b00d6a109f5621f8b.jpg')} alt='Неожиданность.'/>
+				<img
+					src={require("../../resources/Images/e644b78ea1be3d6b00d6a109f5621f8b.jpg")}
+					alt='Неожиданность.'
+				/>
 
-				<figcaption>Путешествия могут принести множество неожиданностей.</figcaption>
-			</figure>               
+				<figcaption>
+					Путешествия могут принести множество неожиданностей.
+				</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/gorjashhie-tury.jpg")}
+					alt='Совместный тур.'
+				/>
+
+				<figcaption>
+					Совместные туры здорово укрепляют отношения.
+				</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/2019-09-16-10.27.28.jpg")}
+					alt='Морское путешествие.'
+				/>
+
+				<figcaption>Путешествия по морю оздоравливают.</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/Palms.png")}
+					alt='Вечерние пальмы.'
+				/>
+
+				<figcaption>Вечерние пальмы.</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/BeneathTheCity.png")}
+					alt='Над городом.'
+				/>
+
+				<figcaption>
+					"А над городом плывут облака; Закрывая небесный свет..."
+				</figcaption>
+			</figure>
 		</aside>
 	);
 }
--- src/Components/BorderElements/RightSideGalleryElements.js
+++ src/Components/BorderElements/RightSideGalleryElements.js
@@ -1,49 +1,114 @@
-import React from 'react';
+import React from "react";
 
 function RightSideGalleryElements() {
-    return (
-		<aside id='rightPanel' className="aside-block gallery border-left">
+	return (
+		<aside id='rightPanel' className='aside-block gallery border-left'>
 			<figure>
-				<img src={require('../../resources/Images/1379633194_orel.jpg')} alt='Орёл.'/>
+				<img
+					src={require("../../resources/Images/1379633194_orel.jpg")}
+					alt='Орёл.'
+				/>
 
 				<figcaption>Орёл.</figcaption>
 			</figure>
 
 			<figure>
-                <img src={require('../../resources/Images/4995833-plyazh-shezlongi-okean-rayskoe-mesto-letniy-otdyh-ekzotika-otpusk-uspokaivayuschie-iyul.jpg')} 
-                     alt='Пляжный курорт.'/>
+				<img
+					src={require("../../resources/Images/4995833-plyazh-shezlongi-okean-rayskoe-mesto-letniy-otdyh-ekzotika-otpusk-uspokaivayuschie-iyul.jpg")}
+					alt='Пляжный курорт.'
+				/>
 
-                <figcaption>Пляжный курорт.</figcaption>
+				<figcaption>Пляжный курорт.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/c7d29a0cd984599082b8fa93cb0097ca.jpg')} alt='Новый год на курорте.'/>
+				<img
+					src={require("../../resources/Images/c7d29a0cd984599082b8fa93cb0097ca.jpg")}
+					alt='Новый год на курорте.'
+				/>
 
 				<figcaption>Новый год — На курорт.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/summer-beach-photo-2560x1600-1003096.jpg')} alt='Летний курорт.'/>
+				<img
+					src={require("../../resources/Images/summer-beach-photo-2560x1600-1003096.jpg")}
+					alt='Летний курорт.'
+				/>
 
-				<figcaption>Лето — лучшее время для отдыха на курорте.</figcaption>
+				<figcaption>
+					Лето — лучшее время для отдыха на курорте.
+				</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/111-1-1.jpg')} alt='Лодочный курорт.'/>
+				<img
+					src={require("../../resources/Images/111-1-1.jpg")}
+					alt='Лодочный курорт.'
+				/>
 
 				<figcaption>Здесь много лодок!</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/23d68b79113b6e348ab17e95500a16cf.jpg')} alt='Замки.'/>
+				<img
+					src={require("../../resources/Images/23d68b79113b6e348ab17e95500a16cf.jpg")}
+					alt='Замки.'
+				/>
 
 				<figcaption>Каждая страна столь индивидуальна.</figcaption>
 			</figure>
 
 			<figure>
-				<img src={require('../../resources/Images/11-1024x768.jpg')} alt='Медитация.'/>
+				<img
+					src={require("../../resources/Images/11-1024x768.jpg")}
+					alt='Медитация.'
+				/>
 
 				<figcaption>Так зачем ограничиваться одной страной?</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/ca3d6bb1c4766837f06ca398632df0b1.jpg")}
+					alt='Одинокий остров.'
+				/>
+
+				<figcaption>
+					В других странах можно найти что угодно.
+				</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/1.jpg")}
+					alt='Качели на острове.'
+				/>
+
+				<figcaption>
+					В некоторых параметрах другие страны похожи на нас.
+				</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/ForestUnderRed.png")}
+					alt='Лес под красным.'
+				/>
+
+				<figcaption>Красиво, не правда ли?</figcaption>
+			</figure>
+
+			<figure>
+				<img
+					src={require("../../resources/Images/UnderUniverse.png")}
+					alt='Под вселенной.'
+				/>
+
+				<figcaption>
+					"Мы все ещё на том же куске Земли, летящем в пространстве
+					бесконечного космоса".
+				</figcaption>
 			</figure>
 		</aside>
 	);
